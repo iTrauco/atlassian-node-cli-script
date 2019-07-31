f2bf410a9d99a8d89d9b8ef262732b88ef73561c
Christopher Trauco <Chris@Trau.co>
2019-07-30 21:17:25 -0400

    user argument input

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-#!/usr/bin/env node
-
-console.log('Helllo World!');
-
-var program = require('commander');
+#!/usr/bin/env node --harmony
+const co = require('co');
+const prompt = require('co-prompt');
+const program = require('commander');
 
 program 
 .arguments('<file>')
 .option('-u, --username <username>', 'The user to authenticate as')
 .option('-p, --password <password>', 'The user\'s password')
 .action(function(file) {
+    co(function *() {
+        var username = yield prompt('username: ');
+        var password = yield prompt.password('password: ');
+    
     console.log('user: %s pass: %s file: %s',
-        program.username, program.password, file);
+        username, password, file);
+
+    });
 })
 
 .parse(process.argv);
